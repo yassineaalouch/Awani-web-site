3c143ca5757e0d3a94e48558f25644b79335d3fe
yassineaalouch <yassine.aalouch@aiac.ma>
2024-10-11 00:53:01 +0100

    try to add google analitics

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 
 import { SessionProvider } from "next-auth/react"
 import { ConverterCurrencyProvider } from '@/components/currencyConverter'
-import GoogleAnalytics from '../components/GoogleAnalytics'
+import Script from 'next/script'
 
 
 export default function App({Component, pageProps: { session, ...pageProps }}) {
@@ -11,7 +11,23 @@ export default function App({Component, pageProps: { session, ...pageProps }}) {
     <CartContextProvider>
       <ConverterCurrencyProvider>
         <SessionProvider session={session}>
-          <GoogleAnalytics/>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script
+            id="google-analytics"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
           <Component {...pageProps}/>
         </SessionProvider>
       </ConverterCurrencyProvider>
