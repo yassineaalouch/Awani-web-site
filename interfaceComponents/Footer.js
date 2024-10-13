acf8d2eb65d994fc8b4eceefff9002e313ad5d7c
yassineaalouch <yassine.aalouch@aiac.ma>
2024-10-13 22:58:32 +0100

    add image products and 2th location

--- interfaceComponents/Footer.js
+++ interfaceComponents/Footer.js
@@ -8,7 +8,7 @@ export default function Footer(props) {
     return (
         <>
         <footer className={`bg-gray-900 mt-0 text-white py-8 ${props.className}`} >
-            <div className="container mx-auto px-4">
+            <div className="container mx-auto px-0">
                 <div className="flex justify-center flex-wrap">
 
                     {/* قسم الروابط السريعة */}
@@ -74,7 +74,24 @@ export default function Footer(props) {
                 </div>
             </div>
             <div className="px-5 mt-10">
-                <MapLocation />
+                    <div className="grid grid-cols-2 text-center gap-5 px-10 my-10">
+                        <div>
+                            <h2 className="pb-3 text-[1.1rem] ">
+                                المحل الأول يقع في تيط مليل قرب مقهى إولي   
+                            </h2>
+                            <div className="border-gray-300 rounded-md border-2">
+                                <MapLocation url ={"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3324.2424337578495!2d-7.527050524303916!3d33.573054173341134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDM0JzIzLjAiTiA3wrAzMScyOC4xIlc!5e0!3m2!1sen!2sma!4v1728853045680!5m2!1sen!2sma"}/>
+                            </div>
+                        </div>
+                        <div>
+                            <h2 className="pb-3 text-[1.1rem] ">
+                                المحل الأول يقع في تيط مليل قرب مقهى إولي   
+                            </h2>
+                            <div className="border-gray-300 rounded-md border-2">
+                                <MapLocation url={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251.9952181389887!2d-7.483418606436457!3d33.54892934301149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda634dfc387e70d%3A0x9d353339dee9142e!2sGGX8%2BHP9%2C%20Bd%20Bassatine%2C%20Titt%20Mellilen!5e0!3m2!1sen!2sma!4v1728420838452!5m2!1sen!2sma"}/>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </footer>
         </>
--- pages/contact.js
+++ pages/contact.js
@@ -65,7 +65,7 @@ export default function ContactUs(){
                 المحل الأول يقع في تيط مليل قرب مقهى إولي   
             </h2>
             <div className="border-gray-300 rounded-md border-2">
-              <MapLocation/>
+              <MapLocation url ={"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3324.2424337578495!2d-7.527050524303916!3d33.573054173341134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzPCsDM0JzIzLjAiTiA3wrAzMScyOC4xIlc!5e0!3m2!1sen!2sma!4v1728853045680!5m2!1sen!2sma"}/>
             </div>
           </div>
           <div>
@@ -73,7 +73,7 @@ export default function ContactUs(){
                المحل الأول يقع في تيط مليل قرب مقهى إولي   
             </h2>
             <div className="border-gray-300 rounded-md border-2">
-              <MapLocation/>
+              <MapLocation url={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251.9952181389887!2d-7.483418606436457!3d33.54892934301149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda634dfc387e70d%3A0x9d353339dee9142e!2sGGX8%2BHP9%2C%20Bd%20Bassatine%2C%20Titt%20Mellilen!5e0!3m2!1sen!2sma!4v1728420838452!5m2!1sen!2sma"}/>
             </div>
           </div>
         </div>
